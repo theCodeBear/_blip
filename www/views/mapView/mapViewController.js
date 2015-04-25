cab49dc41a1b228626fb76f877eacf2b63cd9a1c
Jose Zamudio <zamudio@outlook.com>
2015-04-25 15:33:35 -0700

    Add map to home

--- www/views/mapView/mapViewController.js
+++ www/views/mapView/mapViewController.js
@@ -3,5 +3,27 @@
 angular.module('blip')
 
 .controller('mapViewCtrl', ['$scope', function($scope) {
+  $scope.getLocation = function(){
+
+    //Get the users location and update the map properties
+    if(navigator.geolocation){
+	    navigator.geolocation.getCurrentPosition(function(position){
+          $scope.map.center.latitude = position.coords.latitude;
+          $scope.map.center.longitude = position.coords.longitude;
+      	});
+     }else{
+     	alert('Geolocation not supported by this browser.');
+     }
+  };
+
+  $scope.map = {
+    center: {
+      latitude: 45,
+      longitude: -73
+    },
+    zoom: 14
+  }
+
+  $scope.options = { zoomControl: false, streetViewControl: false, mapTypeControl: false };
 
 }]);
