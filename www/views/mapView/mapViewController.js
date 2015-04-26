6bd39debc7a2be89cceede50c901869a1988b7d3
Jose Zamudio <zamudio@outlook.com>
2015-04-26 03:04:41 -0700

    Real time data

--- www/views/mapView/mapViewController.js
+++ www/views/mapView/mapViewController.js
@@ -6,40 +6,33 @@ angular.module('blip')
 
   var ref = new Firebase('https://blipapp.firebaseio.com/blips');
   var blips = $firebaseArray(ref);
-  var blipArray = [];
 
   $scope.typing = false;
 
   var lat, long, map;
   var posOptions = {timeout: 10000, enableHighAccuracy: true};
   var currentIcon = 'img/blue_dot.png';
-  var heatMapData = [];
+
+  ref.on('value', function(snapshot) {
+    console.log('snapshot', snapshot.val());
+    var heatMapData = []
+    for (var key in snapshot.val()) {
+      heatMapData.push({location: new google.maps.LatLng(snapshot.val()[key].lat, snapshot.val()[key].long)})
+    }
+    heatMapData ? getHeatmap(heatMapData) : console.log('Oleee');
+  });
+
+  function getHeatmap(data) {
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: data
+    });
+    heatmap.setMap(null);
+    heatmap.setMap(map);
+  }
 
   $cordovaGeolocation
   .getCurrentPosition(posOptions)
   .then(function (position) {
-    // lat  = position.coords.latitude;
-    // long = position.coords.longitude;
-    // console.log(JSON.stringify(lat));
-    // console.log(JSON.stringify(long));
-
-    // $scope.map = {
-    //   center: {
-    //     latitude: lat,
-    //     longitude: long
-    //   },
-    //   zoom: 14
-    // }
-
-    // $scope.marker = {
-    //   id: 0,
-    //   coords: {
-    //     latitude: lat,
-    //     longitude: long
-    //   },
-    //   options: { draggable: false }
-    // };
-
     lat = position.coords.latitude;
     long = position.coords.longitude;
     map = showMap(lat,long);
@@ -49,28 +42,7 @@ angular.module('blip')
       map: map,
       icon: currentIcon
     });
-
-    blips.$loaded().then(function() {
-      blips.forEach(function(e) {
-        blipArray.push({ location: new google.maps.LatLng(e.lat, e.long) });
-      });
-    });
-
-    // blipArray.push({ location: new google.maps.LatLng(37.78, -122.4)});
-    // blipArray.push({ location: new google.maps.LatLng(37.79, -122.4)});
-    console.log(blipArray);
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: blipArray
-    });
-    heatmap.setMap(map);
-
-
-  })
-  .then(function(err) {
-    console.log(JSON.stringify(err));
   });
-
-  // $scope.options = { zoomControl: false, streetViewControl: false, mapTypeControl: false };
 
   $scope.blip = function() {
     $scope.typing = true;
@@ -80,23 +52,8 @@ angular.module('blip')
     var obj = {message: message, lat: lat, long: long, sponsor: false}
     blips.$add(obj).then(function(ref) {
       $scope.typing = false;
-      blips.$loaded().then(function() {
-        // blipArray = [];
-        // console.log(blips);
-        // blips.forEach(function(e) {
-          // console.log(e.lat, e.long, e.message);
-          // blipArray.push({ location: new google.maps.LatLng(e.lat, e.long) });
-        // });
-        
-        return map;
-      });
     });
   };
-
-  // var heatMapData = [
-  //   { location: new google.maps.LatLng(37.78, -122.4)},
-  //   { location: new google.maps.LatLng(37.79, -122.4)}
-  // ];
 
   function showMap(myLat, myLong) {
     var mapOptions = {
@@ -105,12 +62,7 @@ angular.module('blip')
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true
     };
-    // var heatmap = new google.maps.visualization.HeatmapLayer({
-    //   data: heatMapData
-    // });
-    console.log('blipArray in showmap', blipArray);
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    // heatmap.setMap(map);
     return map;
   }
 
