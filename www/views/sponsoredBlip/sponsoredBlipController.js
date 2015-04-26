dbe8ad3b8032f962c718f5c7527efa56c26de5b5
Todd Kronenberg <toddkronenberg@gmail.com>
2015-04-25 20:07:18 -0700

    Add basic form design  for sponsored blip

--- www/views/sponsoredBlip/sponsoredBlipController.js
+++ www/views/sponsoredBlip/sponsoredBlipController.js
@@ -4,4 +4,17 @@ angular.module('blip')
 
 .controller('sponsoredBlipCtrl', ['$scope', function($scope) {
 
+  // $scope.stripePayment = function(event) {
+  //   var $form = $(this);
+
+  //   // Disable the submit button to prevent repeated clicks
+  //   $form.find('button').prop('disabled', true);
+
+  //   Stripe.card.createToken($form, stripeResponseHandler);
+
+  //   // Prevent the form from submitting with the default action
+  //   return false;
+  // });
+
+
 }]);
